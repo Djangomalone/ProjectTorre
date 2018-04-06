ffb1f3684cf821d82c38232b16c5f074b82e8936
Djangomalone <lordmips@gmail.com>
2018-04-06 13:47:16 +0200

    Lavoro da Scuola 06/04/2018: login completo

--- Html/LoginDesha.php
+++ Html/LoginDesha.php
@@ -6,8 +6,21 @@
         <link rel="stylesheet" type="text/css" href="../CSS/login.css">
     </head>
     <body>
-        <div id="Login">
-            
-        </div>
+         <form method="post" action="MainPage.php">
+            Email:
+            <input type="text" name="email" value="">
+            <br>
+            Password:
+            <input type="password" name="password" value="">
+            <br>
+            <input type="radio" name="radioSelezione" value="Maestro">
+            Maestro
+            <input type="radio" name="radioSelezione" value="Utente">
+            Utente
+            <br>
+            <button type="submit" name="login">
+                Accedi
+            </button>
+        </form>
     </body>
 </html>
--- Html/MainPage.php
+++ Html/MainPage.php
@@ -1,3 +1,35 @@
-<?php
-    include('dbconnection.php');
-?>
+<html>
+    <body>
+        <?php
+            include('dbconnection.php');
+            $varemail = mysqli_real_escape_string($connection, $_POST['email']);
+            $varpassword = mysqli_real_escape_string($connection, $_POST['password']);
+            if($_POST['radioSelezione']=='Utente'){
+                $ResultU = mysqli_query($connection, "SELECT Email, Password FROM utente WHERE BINARY Email = '$varemail' AND BINARY Password='$varpassword'");
+                echo "Vediamo se sei entrato: <br>";
+                $NRigheU = mysqli_num_rows($ResultU);
+                if($NRigheU > 0){
+                    while($rowU = $ResultU->fetch_assoc()){
+                        echo "Benvenuto utente!<br>";
+                    }
+                }
+                else{
+                    echo "LOGIN ERRATO: EMAIL O PASSWORD NON CORRETTI";
+                }
+            }
+            if($_POST['radioSelezione']=='Maestro'){
+                $ResultA = mysqli_query($connection, "SELECT Email, Password FROM admin WHERE BINARY Email = '$varemail' AND BINARY Password='$varpassword'");
+                echo "Vediamo se sei entrato: <br>";
+                $NRigheA = mysqli_num_rows($ResultA);
+                if($NRigheA > 0){
+                    while($rowA = $ResultA->fetch_assoc()){
+                        echo "Benvenuto maestro!<br>";
+                    }
+                }
+                else{
+                    echo "LOGIN ERRATO: EMAIL O PASSWORD NON CORRETTI";
+                }
+            }    
+        ?>
+    </body>
+</html>
