ccd259bc17bca6f9ffc74dc8e7679f62180d6ad5
Djangomalone <lordmips@gmail.com>
2018-05-04 17:38:13 +0200

    Lavoro da Scuola 04/05/2018

--- Html/MakePrenotazioni.php
+++ Html/MakePrenotazioni.php
@@ -11,15 +11,16 @@
         echo "<br>";
         $Result = mysqli_query($connection, "SELECT Id_Lezione, Data_Lezione, Ora_Lezione, NomeAdmin FROM lezione JOIN admin ON (lezione.Id_Admin_Lezione = admin.Id_Admin) WHERE Data_Lezione > '$Data'");
         $NRighe = mysqli_num_rows($Result);
-        echo "<table><tr><th>Data</th><th>Ora</th><th>Tenuta da</th></tr>";
+        echo "<form><table><tr><th>Data</th><th>Ora</th><th>Tenuta da</th><th>Scegli</th></tr>";
         if($NRighe > 0){
             while($row = $Result->fetch_assoc()){
-                echo "<td>" . $row["Data_Lezione"] . "</td><td>" . $row["Ora_Lezione"] . "</td><td>" . $row["NomeAdmin"] ."</td></tr>";
+                $DataGirata = date("d-m-Y", strtotime($row["Data_Lezione"]));
+                echo "<tr><td>" . $DataGirata . "</td><td>" . $row["Ora_Lezione"] . "</td><td>" . $row["NomeAdmin"] ."</td> <td><button type='submit' name='bottone". $row["Id_Lezione"] ."'>Seleziona!</button></td></tr>";
             }
         }
         else{
             echo "Nessun Risultato";
         }
-        echo "</table>";
+        echo "</table></form>";
     ?>
 </html>
